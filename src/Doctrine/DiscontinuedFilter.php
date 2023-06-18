135902faa623e5ba60dd3b249fd547c5d8c2bcf6
Petre Ro <petrero@gmail.com>
2023-06-18 15:44:04 +0300

    13.4 Adding the Filter Logoc  - As I mentioned, this will be called for every query. So we need to be careful to only add our WHERE clause when we're querying for fortune cookies. To do that, say if $targetEntity->name !== FortuneCookie::class, then return ''.
     This method returns a string... and that string is basically added to a WHERE clause. At the bottom, return sprintf('%s.discontinued = false'), passing $targetTableAlias for the wildcard.
    
     Ready to check this out? On the homepage, the "Proverbs" count should go from 3 to 2. And... it does! Check out the query for this. Yup! It has t0.discontinued = false inside of every query for fortune cookies. That's awesome!

--- src/Doctrine/DiscontinuedFilter.php
+++ src/Doctrine/DiscontinuedFilter.php
@@ -2,12 +2,17 @@
 
 namespace App\Doctrine;
 
+use App\Entity\FortuneCookie;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Filter\SQLFilter;
 
 class DiscontinuedFilter extends SQLFilter {
 	public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias): string {
-		dd($targetEntity, $targetTableAlias);
+		if ($targetEntity->name !== FortuneCookie::class) {
+			return '';
+		};
+
+		return sprintf('%s.discontinued = false', $targetTableAlias);
 	}
 
 }
